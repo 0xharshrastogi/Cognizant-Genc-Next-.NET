f0bd3f8e75271ca0cec649c65d75a173a5fa3558
Harsh Rastogi <rastogiharsh04@gmail.com>
2022-03-08 14:26:01 +0530

    Age Calculation Added

---
+++ C# Programming/Lambda Expressions, Access Modifiers, Lists, Arraylists vs Lists vs Arrays, Structs, Enums, Math Class, Random Class, Regular Expressions, DateTime, Linq, Threads/Age Calculation/Program.cs
@@ -0,0 +1,22 @@
+using System.Linq;
+using System;
+
+namespace DateEx1
+{
+    public class Program
+    {
+        public static void Main(string[] args)    //DO NOT CHANGE the 'Main' method signature
+        {
+            Console.WriteLine("Enter the date of birth (dd-mm-yyyy): ");
+            var dobText = Console.ReadLine();
+            var age = calculateAge(dobText);
+            Console.WriteLine(age);
+        }
+
+        public static int calculateAge(string dateOfBirth)
+        {
+            var timespan = DateTime.Now - DateTime.Parse(String.Join("-", dateOfBirth.Split("-").Reverse()));
+            return timespan.Days / 365;
+        }
+    }
+}
