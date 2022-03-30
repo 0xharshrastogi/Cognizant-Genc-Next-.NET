c8d1bc35b605ff0b6f97bf9ca3d94d7fb5d47129
Harsh Rastogi <rastogiharsh04@gmail.com>
2022-03-30 16:11:42 +0530

    Minimax Added

---
+++ Data Structures and Algorithms/Mini-Max Sum/Program.cs
@@ -0,0 +1,43 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+    public static void miniMaxSum(List<int> arr)
+    {
+        long sum = 0,
+            min = long.MaxValue,
+            max = long.MinValue;
+
+        foreach (var value in arr)
+        {
+            sum += value;
+            max = Math.Max(max, value);
+            min = Math.Min(min, value);
+        }
+
+        Console.WriteLine($"{sum - max} {sum - min}");
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        Result.miniMaxSum(arr);
+    }
+}
