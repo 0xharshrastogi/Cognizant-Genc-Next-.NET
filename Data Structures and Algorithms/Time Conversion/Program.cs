f63623c4e97436d9464761042664755191e032cb
Harsh Rastogi <rastogiharsh04@gmail.com>
2022-04-05 22:05:42 +0530

    Time Conversion Added

---
+++ Data Structures and Algorithms/Time Conversion/Program.cs
@@ -0,0 +1,40 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    public static string timeConversion(string s)
+    {
+        var time = DateTime.ParseExact(s, "hh:mm:sstt", null);
+        return time.ToString("HH:mm:ss");
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string s = Console.ReadLine();
+
+        string result = Result.timeConversion(s);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
