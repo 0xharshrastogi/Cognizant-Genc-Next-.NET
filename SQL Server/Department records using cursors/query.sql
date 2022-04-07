7784b66201c37eccd5dfda0a514cd001af39d1a4
Harsh Rastogi <rastogiharsh04@gmail.com>
2022-04-07 10:25:05 +0530

    Cursor Hands On Added

---
+++ SQL Server/Department records using cursors/query.sql
@@ -0,0 +1,33 @@
+-------- Creating Cursor-------------
+DECLARE dbcursor CURSOR 
+FOR SELECT
+  deptname, COUNT(*)
+FROM
+  Employee E,
+  Department D
+WHERE
+    E.workdept=D.deptno
+GROUP BY
+    deptname
+HAVING 
+      COUNT(workdept) > 1
+ORDER BY 
+      deptname
+-------- Cursor Created-------------
+
+DECLARE @name NVARCHAR(36)
+DECLARE @count INT
+
+OPEN dbcursor
+
+FETCH NEXT FROM dbcursor INTO @name, @count
+
+WHILE @@FETCH_STATUS=0
+BEGIN
+  PRINT CONCAT(@name, ' department has ', @count, ' employees')
+  FETCH NEXT FROM dbcursor INTO @name, @count
+END
+
+CLOSE dbcursor
+DEALLOCATE dbcursor
+GO
