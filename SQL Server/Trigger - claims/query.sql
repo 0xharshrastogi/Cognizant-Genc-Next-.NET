5a50a4c2db77a987e3068e45cafaac47e3406efa
Harsh Rastogi <rastogiharsh04@gmail.com>
2022-04-13 18:20:05 +0530

    Trigger Claim Fixxed

--- SQL Server/Trigger - claims/query.sql
+++ SQL Server/Trigger - claims/query.sql
@@ -1,36 +1,18 @@
-CREATE trigger claim_audits
-ON claims
+CREATE trigger claim_audits 
+ON claims 
 AFTER INSERT
 AS
 BEGIN
-  INSERT INTO [claim_audit]
-  SELECT
-    C.first_name,
-    SUM(I.amount_of_claim),
-    'Updated customer claimed amount'
-  FROM Inserted I
-    JOIN [customer_policy] CP
-    ON I.customer_policy_id=CP.ID
-    JOIN [customer] C
-    ON CP.customer_id=C.id
-  GROUP BY C.first_name
+  INSERT INTO claim_audit
+  SELECT CT.first_name, SUM(C.amount_of_claim), 'Updated customer claimed amount'
+  FROM claims C
+    JOIN customer_policy CP
+    ON C.customer_policy_id=CP.id
+    JOIN customer CT
+    ON CP.customer_id=CT.id
+  WHERE 
+    C.customer_policy_id IN (SELECT customer_policy_id
+  FROM inserted)
+  GROUP BY CT.first_name
 END
 GO
-
-SELECT
-  C.id,
-  C.first_name,
-  SUM(amount_of_claim) amount_of_claim
-FROM
-  customer C
-  JOIN customer_policy CP
-  ON CP.customer_id=C.id
-  JOIN claims CL
-  ON CL.customer_policy_id=CP.id
-GROUP BY 
-    C.id, C.first_name
-GO
-
-SELECT SUM(amount_of_claim)
-FROM claims
-GROUP BY id
