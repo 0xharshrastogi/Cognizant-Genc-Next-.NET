15d920aa902d7159ff95a5e7159f27a507be6b56
Harsh Rastogi <rastogiharsh04@gmail.com>
2022-05-05 16:14:43 +0530

    03-01-C#4.5-Handson

---
+++ Dotnet Advanced Features/Code Analysis/03-01-C#4.5-Handson/App/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+
+static async Task<string> GetString()
+{
+    await Task.Delay(3000);
+
+    return "Hello World";
+}
+
+static async void ProgramFunc()
+{
+    var result = await GetString();
+    Console.WriteLine(result);
+}
+
+// -----------------------------------------------------------------------------------
+
+static void GetCohertDetails(string name, int count, string mode, string track, string currModule)
+{
+    Console.WriteLine("And print the details in the format – It is [Cohort name] with [GenC count] GenCs undergoing training for [Track] thru [Mode]. The current module of training is [Current Module]");
+}
+
+GetCohertDetails(
+    name: "FSE DOTNET",
+    mode: "OBL",
+    count: 20,
+    track: ".NET",
+    currModule: ".NET Advance"
+);
