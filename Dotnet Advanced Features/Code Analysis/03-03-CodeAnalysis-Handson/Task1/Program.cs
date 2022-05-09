b27adfd9fd49799e6e85bc3dbb50496c796b82c4
Harsh Rastogi <rastogiharsh04@gmail.com>
2022-05-09 07:55:49 +0530

    Code Analysis 3

---
+++ Dotnet Advanced Features/Code Analysis/03-03-CodeAnalysis-Handson/Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        public string? Name { get; set; }
+
+        public static void Main()
+        {
+            string[] names = new string[] { "James", "Bodgy", "Chirag", "Denis", "Alfee", "Bond" };
+            IEnumerable<string> namesWithB = names.Where((name) => name.StartsWith('B'));
+        }
+    }
+}
---
+++ Dotnet Advanced Features/Code Analysis/03-03-CodeAnalysis-Handson/Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Task2
+{
+    internal static class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Enter a N value to print");
+            int num = int.Parse(Console.ReadLine()!);
+
+            for (int i = 0; i < num; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
