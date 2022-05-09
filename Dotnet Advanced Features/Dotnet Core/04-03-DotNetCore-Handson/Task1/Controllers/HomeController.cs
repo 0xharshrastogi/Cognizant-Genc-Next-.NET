f94a8da9dd216411bf89fa74f5618d500b820045
Harsh Rastogi <rastogiharsh04@gmail.com>
2022-05-09 08:57:34 +0530

    Razor App Done

---
+++ Dotnet Advanced Features/Dotnet Core/04-03-DotNetCore-Handson/Task1/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System.Diagnostics;
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+using Task1.Models;
+
+namespace Task1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public ActionResult ITOrganizations()
+        {
+            return View(new List<string>
+            {
+                "www.microsoft.com", "www.cognizant.com", "Google", "Amazon", "TCS"
+            });
+        }
+
+        public IActionResult GetEmployeeList()
+        {
+            string? blob = System.IO.File.ReadAllText("Data/Employee.json");
+            IEnumerable<Employee> employees = JsonSerializer.Deserialize<IEnumerable<Employee>>(
+                blob,
+                new JsonSerializerOptions(JsonSerializerDefaults.Web)
+            )!;
+            return View(employees);
+        }
+    }
+}
---
+++ Dotnet Advanced Features/Dotnet Core/04-03-DotNetCore-Handson/Task1/Models/Employee.cs
@@ -0,0 +1,12 @@
+#nullable disable
+
+namespace Task1.Models
+{
+    internal class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+        public bool Permanent { get; set; }
+    }
+}
